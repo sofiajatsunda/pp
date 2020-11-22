38814fce3eb76edc78be97a2b09f8402677d3c96
sofia <32436152+sofiajatsunda@users.noreply.github.com>
2020-11-22 21:04:11 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from wsgiref.simple_server import make_server
+app = Flask(__name__)
+
+@app.route('/api/v1/hello-world-24')
+def hello_world():
+    return 'Hello World 24!'
+
+with make_server('',5000,app)as server:
+    print("Port 127.0.0.1:5000")
+
+    server.serve_forever()
+
+    #http://127.0.0.1:5000/api/v1/hello-world-24
